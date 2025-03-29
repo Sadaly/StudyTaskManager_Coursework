016a04cca5f05da0f6e62e3e719ab944d9c7e4b9
DP1415 <Daniil-2005-zima@mail.ru>
2025-03-29 11:35:00 +0300

    Добавление проверки на наличии пользователя в группе при создании приглашения

--- StudyTaskManager/StudyTaskManager.Persistence/Repository/GroupInviteRepository.cs
+++ StudyTaskManager/StudyTaskManager.Persistence/Repository/GroupInviteRepository.cs
@@ -9,7 +9,29 @@ namespace StudyTaskManager.Persistence.Repository
     class GroupInviteRepository : Generic.TRepository<GroupInvite>, IGroupInviteRepository
     {
         public GroupInviteRepository(AppDbContext dbContext) : base(dbContext) { }
-        // TODO добавить проверку наличия пользователя в чате при попытке добавить приглашение
+
+        public override async Task<Result> AddAsync(GroupInvite groupInvite, CancellationToken cancellationToken = default)
+        {
+            // Проверка наличия пользователя в чате при попытке добавить приглашение
+            UserInGroup? uig = await _dbContext.Set<UserInGroup>()
+                .FirstOrDefaultAsync(uig => uig.GroupId == groupInvite.GroupId && uig.UserId == groupInvite.ReceiverId);
+
+            if (uig != null)
+                return Result.Failure(new Error(
+                    $"{typeof(UserInGroup)}.TheUserIsAlreadyInTheGroup",
+                    $"Пользователь(" +
+                    $"{(groupInvite.Receiver == null ?
+                        groupInvite.ReceiverId :
+                        groupInvite.Receiver.Username)}" +
+                    $") уже в группе(" +
+                    $"{(groupInvite.Group == null ?
+                        groupInvite.GroupId :
+                        groupInvite.Group.Title)}"));
+
+            await _dbContext.Set<GroupInvite>().AddAsync(groupInvite, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return Result.Success();
+        }
         public async Task<Result<List<GroupInvite>>> GetByUserAsync(Group group, CancellationToken cancellationToken = default)
         {
             return await _dbContext.Set<GroupInvite>()
