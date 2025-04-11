85728d3f27980bfb50e48c1e9b831f592b807b3a
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-11 11:04:16 +0300

    Изменение команды изменения номера телефона у пользователя

--- StudyTaskManager/StudyTaskManager.Application/Entity/Users/Commands/UserChangePhoneNumber/UserChangePhoneNumberCommandHandler.cs
+++ StudyTaskManager/StudyTaskManager.Application/Entity/Users/Commands/UserChangePhoneNumber/UserChangePhoneNumberCommandHandler.cs
@@ -8,39 +8,35 @@ using StudyTaskManager.Domain.ValueObjects;
 
 namespace StudyTaskManager.Application.Entity.Users.Commands.UserChangePhoneNumber
 {
-	internal sealed class UserChangePhoneNumberCommandHandler : ICommandHandler<UserChangePhoneNumberCommand, Guid>
-	{
-		private readonly IUnitOfWork _unitOfWork;
-		private readonly IUserRepository _userRepository;
+    internal sealed class UserChangePhoneNumberCommandHandler : ICommandHandler<UserChangePhoneNumberCommand, Guid>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserRepository _userRepository;
 
-		public UserChangePhoneNumberCommandHandler(IUnitOfWork unitOfWork, IUserRepository userRepository)
-		{
-			_unitOfWork = unitOfWork;
-			_userRepository = userRepository;
-		}
+        public UserChangePhoneNumberCommandHandler(IUnitOfWork unitOfWork, IUserRepository userRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = userRepository;
+        }
 
-		public async Task<Result<Guid>> Handle(UserChangePhoneNumberCommand request, CancellationToken cancellationToken)
-		{
-			Result<PhoneNumber> phoneNumberResult = PhoneNumber.Create(request.NewPhoneNumber);
+        public async Task<Result<Guid>> Handle(UserChangePhoneNumberCommand request, CancellationToken cancellationToken)
+        {
+            Result<PhoneNumber> phoneNumber = PhoneNumber.Create(request.NewPhoneNumber);
+            if (phoneNumber.IsFailure) return Result.Failure<Guid>(phoneNumber.Error);
 
-			if (!(await _userRepository.IsPhoneNumberUniqueAsync(phoneNumberResult.Value, cancellationToken)).Value)
-			{
-				return Result.Failure<Guid>(PersistenceErrors.User.PhoneNumberAlreadyInUse);
-			}
+            Result<bool> isPhoneNumberUnique = await _userRepository.IsPhoneNumberUniqueAsync(phoneNumber.Value, cancellationToken);
+            if (isPhoneNumberUnique.IsFailure) return Result.Failure<Guid>(isPhoneNumberUnique.Error);
+            if (!isPhoneNumberUnique.Value) return Result.Failure<Guid>(PersistenceErrors.User.PhoneNumberAlreadyInUse);
 
-			var foundUserResult = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
-			if (foundUserResult.IsFailure)
-			{
-				return Result.Failure<Guid>(foundUserResult.Error);
-			}
+            Result<Domain.Entity.User.User> user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+            if (user.IsFailure) return Result.Failure<Guid>(user.Error);
 
-			var user = foundUserResult.Value;
-			user.ChangePhoneNumber(phoneNumberResult.Value);
+            user.Value.ChangePhoneNumber(phoneNumber.Value);
 
-			await _userRepository.UpdateAsync(user, cancellationToken);
-			await _unitOfWork.SaveChangesAsync(cancellationToken);
+            await _userRepository.UpdateAsync(user.Value, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-			return user.Id;
-		}
-	}
+            return user.Value.Id;
+        }
+    }
 }
