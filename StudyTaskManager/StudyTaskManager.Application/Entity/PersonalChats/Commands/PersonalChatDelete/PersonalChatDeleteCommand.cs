dea6dcfa36709768d1330c2b5669efcd8680e2b9
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-15 10:14:41 +0300

    Удалил ненужные handler-ы, добавил команду удаления PersonalChat

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalChats/Commands/PersonalChatDelete/PersonalChatDeleteCommand.cs
@@ -0,0 +1,6 @@
+﻿using StudyTaskManager.Application.Entity.Generic.Commands.DeleteById;
+
+namespace StudyTaskManager.Application.Entity.PersonalChats.Commands.PersonalChatDelete;
+
+public sealed record PersonalChatDeleteCommand(
+    Guid PersonalChatId) : DeleteByIdCommand<Domain.Entity.User.Chat.PersonalChat>(PersonalChatId);
--- StudyTaskManager/StudyTaskManager.Application/Entity/PersonalMessages/Commands/PersonalMessageDelete/PersonalMessageDeleteCommand.cs
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalMessages/Commands/PersonalMessageDelete/PersonalMessageDeleteCommand.cs
@@ -1,6 +1,8 @@
 ﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Application.Entity.Generic.Commands.DeleteById;
+using StudyTaskManager.Domain.Entity.User.Chat;
 
 namespace StudyTaskManager.Application.Entity.PersonalMessages.Commands.PersonalMessageDelete;
 
 public sealed record PersonalMessageDeleteCommand(
-    Guid PersonalMessageId) : ICommand;
+    Guid PersonalMessageId) : DeleteByIdCommand<PersonalMessage>(PersonalMessageId);
