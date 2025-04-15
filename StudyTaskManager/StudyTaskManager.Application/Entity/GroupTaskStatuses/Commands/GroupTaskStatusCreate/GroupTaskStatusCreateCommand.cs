274451485bbc783a79ad98b13bca9ead184f58e1
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-15 21:29:25 +0300

    Реализация части команд GroupTaskStatuses

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupTaskStatuses/Commands/GroupTaskStatusCreate/GroupTaskStatusCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+
+namespace StudyTaskManager.Application.Entity.GroupTaskStatuses.Commands.GroupTaskStatusCreate;
+
+public sealed record GroupTaskStatusCreateCommand(
+    string Title,
+    bool CanBeUpdated,
+    Guid? GroupId,
+    string? Description) : ICommand<Guid>;
