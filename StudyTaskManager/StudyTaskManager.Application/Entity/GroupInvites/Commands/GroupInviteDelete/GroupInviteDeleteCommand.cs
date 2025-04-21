364d04353e9d7dfe845c532beef016fce07c681c
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-21 23:29:54 +0300

    Реализация удаления(по user & group ids) приглашения

--- StudyTaskManager/StudyTaskManager.Application/Entity/GroupInvites/Commands/GroupInviteDelete/GroupInviteDeleteCommand.cs
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupInvites/Commands/GroupInviteDelete/GroupInviteDeleteCommand.cs
@@ -1,6 +1,7 @@
-﻿namespace StudyTaskManager.Application.Entity.GroupInvites.Commands.GroupInviteDelete
-{
-    internal class GroupInviteDeleteCommand
-    {
-    }
-}
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+
+namespace StudyTaskManager.Application.Entity.GroupInvites.Commands.GroupInviteDelete;
+
+public sealed record GroupInviteDeleteCommand(
+    Guid ReceiverId,
+    Guid GroupId) : ICommand;
--- StudyTaskManager/StudyTaskManager.Application/Entity/GroupInvites/Commands/GroupInviteDelete/GroupInviteDeleteCommandHandler.cs
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupInvites/Commands/GroupInviteDelete/GroupInviteDeleteCommandHandler.cs
@@ -1,6 +1,42 @@
-﻿namespace StudyTaskManager.Application.Entity.GroupInvites.Commands.GroupInviteDelete
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Abstractions;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.GroupInvites.Commands.GroupInviteDelete
 {
-    internal class GroupInviteDeleteCommandHandler
+    public class GroupInviteDeleteCommandHandler : ICommandHandler<GroupInviteDeleteCommand>
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserRepository _userRepository;
+        private readonly IGroupRepository _groupRepository;
+        private readonly IGroupInviteRepository _groupInviteRepository;
+
+        public GroupInviteDeleteCommandHandler(IUnitOfWork unitOfWork, IUserRepository userRepository, IGroupRepository groupRepository, IGroupInviteRepository groupInviteRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = userRepository;
+            _groupRepository = groupRepository;
+            _groupInviteRepository = groupInviteRepository;
+        }
+
+        public async Task<Result> Handle(GroupInviteDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.ReceiverId, cancellationToken);
+            if (user.IsFailure) return user;
+
+            var group = await _groupRepository.GetByIdAsync(request.GroupId, cancellationToken);
+            if (group.IsFailure) return group;
+
+            var invite = await _groupInviteRepository.GetByUserAndGropu(user.Value, group.Value, cancellationToken);
+            if (invite.IsFailure) return invite;
+
+            var remove = await _groupInviteRepository.RemoveAsync(invite.Value, cancellationToken);
+            if (remove.IsFailure) return remove;
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
     }
 }
