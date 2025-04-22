ad845898ff76884995c6e49917ca7a7fd3bbaa79
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-22 20:23:47 +0300

    Добавли запрос на получениче статусов задачи для группы вместе с общими (получение только общих кмк не требует отдельного запроса, получение только групповых уместнее сделать кмк в группе)

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupTaskStatuses/Queries/GroupTaskStatusGetByGroupWithBase/GroupTaskStatusGetByGroupWithBaseQuery.cs
@@ -0,0 +1,8 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+
+namespace StudyTaskManager.Application.Entity.GroupTaskStatuses.Queries.GroupTaskStatusGetByGroupWithBase;
+
+public sealed record GroupTaskStatusGetByGroupWithBaseQuery(
+    Guid GroupId,
+    int StartIndex,
+    int Count) : IQuery<List<GroupTaskStatusGetByGroupWithBaseRepsonseElements>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupTaskStatuses/Queries/GroupTaskStatusGetByGroupWithBase/GroupTaskStatusGetByGroupWithBaseRepsonseElements.cs
@@ -0,0 +1,20 @@
+﻿using StudyTaskManager.Domain.Entity.Group.Task;
+
+namespace StudyTaskManager.Application.Entity.GroupTaskStatuses.Queries.GroupTaskStatusGetByGroupWithBase;
+
+public sealed record GroupTaskStatusGetByGroupWithBaseRepsonseElements(
+    Guid Id,
+    string Name,
+    Guid? GroupId,
+    bool CanBeUpdated,
+    string? Description)
+{
+    internal GroupTaskStatusGetByGroupWithBaseRepsonseElements(GroupTaskStatus status)
+        : this(
+             status.Id,
+             status.Name.Value,
+             status.GroupId,
+             status.CanBeUpdated,
+             status.Description?.Value)
+    { }
+}
