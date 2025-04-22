a8f7ac6e26c1268b5d17fde00257783f2cc2148c
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-22 20:59:30 +0300

    Добавил команду удаления статуса задачи по id

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupTaskStatuses/Commands/GroupTaskStatusDelete/GroupTaskStatusDeleteCommand.cs
@@ -0,0 +1,5 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+
+namespace StudyTaskManager.Application.Entity.GroupTaskStatuses.Commands.GroupTaskStatusDelete;
+
+public sealed record GroupTaskStatusDeleteCommand(Guid Id) : ICommand;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupTaskStatuses/Commands/GroupTaskStatusDelete/GroupTaskStatusDeleteCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.GroupTaskStatuses.Commands.GroupTaskStatusDelete
+{
+    public class GroupTaskStatusDeleteCommandHandler : ICommandHandler<GroupTaskStatusDeleteCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGroupTaskStatusRepository _groupTaskStatusRepository;
+
+        public GroupTaskStatusDeleteCommandHandler(IUnitOfWork unitOfWork, IGroupTaskStatusRepository groupTaskStatusRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _groupTaskStatusRepository = groupTaskStatusRepository;
+        }
+
+        public async Task<Result> Handle(GroupTaskStatusDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var remove = await _groupTaskStatusRepository.RemoveAsync(request.Id, cancellationToken);
+            if (remove.IsFailure) return Result.Failure(remove);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
