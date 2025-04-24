d3c41a4a96fdc6609d321f29a9b50d68ccaee09c
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-24 12:16:58 +0300

    Реализовал PersonalMessageTakeQuery

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalMessages/Queries/PersonalMessageTake/PersonalMessageTakeQuery.cs
@@ -0,0 +1,10 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Entity.User.Chat;
+using System.Linq.Expressions;
+
+namespace StudyTaskManager.Application.Entity.PersonalMessages.Queries.PersonalMessageTake;
+
+public sealed record PersonalMessageTakeQuery(
+    int StartIndex,
+    int Count,
+    Expression<Func<PersonalMessage, bool>>? Predicate) : IQuery<List<PersonalMessageResponse>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalMessages/Queries/PersonalMessageTake/PersonalMessageTakeQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.PersonalMessages.Queries.PersonalMessageTake
+{
+    public class PersonalMessageTakeQueryHandler : IQueryHandler<PersonalMessageTakeQuery, List<PersonalMessageResponse>>
+    {
+        private readonly IPresonalMessageRepository _presonalMessageRepository;
+
+        public PersonalMessageTakeQueryHandler(IPresonalMessageRepository presonalMessageRepository)
+        {
+            _presonalMessageRepository = presonalMessageRepository;
+        }
+
+        public async Task<Result<List<PersonalMessageResponse>>> Handle(PersonalMessageTakeQuery request, CancellationToken cancellationToken)
+        {
+            var messages = request.Predicate == null
+                ? await _presonalMessageRepository.GetAllAsync(request.StartIndex, request.Count, cancellationToken)
+                : await _presonalMessageRepository.GetAllAsync(request.StartIndex, request.Count, request.Predicate, cancellationToken);
+
+            if (messages.IsFailure) return Result.Failure<List<PersonalMessageResponse>>(messages);
+
+            return messages.Value.Select(pm => new PersonalMessageResponse(pm)).ToList();
+        }
+    }
+}
