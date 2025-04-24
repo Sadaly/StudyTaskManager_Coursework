ac9dac37c65d4770d0473199d0009a6487ad2111
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-24 13:42:29 +0300

    Реализовал PersonalChatTakeQuery

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalChats/Queries/PersonalChatTake/PersonalChatTakeQuery.cs
@@ -0,0 +1,10 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Entity.User.Chat;
+using System.Linq.Expressions;
+
+namespace StudyTaskManager.Application.Entity.PersonalChats.Queries.PersonalChatTake;
+
+public sealed record PersonalChatTakeQuery(
+    int StartIndex,
+    int Count,
+    Expression<Func<PersonalChat, bool>> Perdicate) : IQuery<List<PersonalChatsResponse>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalChats/Queries/PersonalChatTake/PersonalChatTakeQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.PersonalChats.Queries.PersonalChatTake
+{
+    public class PersonalChatTakeQueryHandler : IQueryHandler<PersonalChatTakeQuery, List<PersonalChatsResponse>>
+    {
+        private readonly IPersonalChatRepository _personalChatRepository;
+
+        public PersonalChatTakeQueryHandler(IPersonalChatRepository personalChatRepository)
+        {
+            _personalChatRepository = personalChatRepository;
+        }
+
+        public async Task<Result<List<PersonalChatsResponse>>> Handle(PersonalChatTakeQuery request, CancellationToken cancellationToken)
+        {
+            var chats = request.Perdicate == null
+                ? await _personalChatRepository.GetAllAsync(request.StartIndex, request.Count, cancellationToken)
+                : await _personalChatRepository.GetAllAsync(request.StartIndex, request.Count, request.Perdicate, cancellationToken);
+
+            if (chats.IsFailure) return Result.Failure<List<PersonalChatsResponse>>(chats);
+
+            return chats.Value.Select(pc => new PersonalChatsResponse(pc)).ToList();
+        }
+    }
+}
