2c23fc74d2842dfac34142e5e6840483bf08491e
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-24 14:13:56 +0300

    Реализовал GroupInviteGetAllQuery

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupInvites/Queries/GroupInviteGetAll/GroupInviteGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Entity.Group;
+using System.Linq.Expressions;
+
+namespace StudyTaskManager.Application.Entity.GroupInvites.Queries.GroupInviteGetAll;
+
+public sealed record GroupInviteGetAllQuery(
+    Expression<Func<GroupInvite, bool>> Perdicate) : IQuery<List<GroupInviteResponse>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupInvites/Queries/GroupInviteGetAll/GroupInviteGetAllQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.DomainEvents;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.GroupInvites.Queries.GroupInviteGetAll
+{
+    public class GroupInviteGetAllQueryHandler : IQueryHandler<GroupInviteGetAllQuery, List<GroupInviteResponse>>
+    {
+        private readonly IGroupInviteRepository _groupInviteRepository;
+
+        public GroupInviteGetAllQueryHandler(IGroupInviteRepository groupInviteRepository)
+        {
+            _groupInviteRepository = groupInviteRepository;
+        }
+
+        public async Task<Result<List<GroupInviteResponse>>> Handle(GroupInviteGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var invites = request.Perdicate == null
+                ? await _groupInviteRepository.GetAllAsync(cancellationToken)
+                : await _groupInviteRepository.GetAllAsync(request.Perdicate, cancellationToken);
+
+            if (invites.IsFailure) return Result.Failure<List<GroupInviteResponse>>(invites);
+
+            return invites.Value.Select(gi => new GroupInviteResponse(gi)).ToList();
+        }
+    }
+}
