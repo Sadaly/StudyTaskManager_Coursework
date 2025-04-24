9ee31ed69697c59d2a58efe26e52904a788c4bd2
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-24 12:01:01 +0300

    Реализовал SystemRoleGetAllQuery

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/SystemRoles/Queries/SystemRoleGetAll/SystemRoleGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Entity.User;
+using System.Linq.Expressions;
+
+namespace StudyTaskManager.Application.Entity.SystemRoles.Queries.SystemRoleGetAll;
+
+public sealed record SystemRoleGetAllQuery(
+    Expression<Func<SystemRole, bool>>? Predicate) : IQuery<List<SystemRoleResponse>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/SystemRoles/Queries/SystemRoleGetAll/SystemRoleGetAllQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.SystemRoles.Queries.SystemRoleGetAll
+{
+    public class SystemRoleGetAllQueryHandler : IQueryHandler<SystemRoleGetAllQuery, List<SystemRoleResponse>>
+    {
+        private readonly ISystemRoleRepository _systemRoleRepository;
+
+        public SystemRoleGetAllQueryHandler(ISystemRoleRepository systemRoleRepository)
+        {
+            _systemRoleRepository = systemRoleRepository;
+        }
+
+        public async Task<Result<List<SystemRoleResponse>>> Handle(SystemRoleGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var roles = request.Predicate == null
+                ? await _systemRoleRepository.GetAllAsync(cancellationToken)
+                : await _systemRoleRepository.GetAllAsync(request.Predicate, cancellationToken);
+
+            if (roles.IsFailure) return Result.Failure<List<SystemRoleResponse>>(roles);
+
+            var listRes = roles.Value.Select(r => new SystemRoleResponse(r)).ToList();
+
+            return listRes;
+        }
+    }
+}
