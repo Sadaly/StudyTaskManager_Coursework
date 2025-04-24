5ab03b6b9ab928f774641f0ba911772713eb1a43
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-24 14:09:34 +0300

    Реализовал GroupRoleGetAllQuery

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupRoles/Queries/GroupRoleGetAll/GroupRoleGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Entity.Group;
+using System.Linq.Expressions;
+
+namespace StudyTaskManager.Application.Entity.GroupRoles.Queries.GroupRoleGetAll;
+
+public sealed record GroupRoleGetAllQuery(
+    Expression<Func<GroupRole, bool>> Perdicate) : IQuery<List<GroupRoleResponse>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupRoles/Queries/GroupRoleGetAll/GroupRoleGetAllQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.GroupRoles.Queries.GroupRoleGetAll
+{
+    public class GroupRoleGetAllQueryHandler : IQueryHandler<GroupRoleGetAllQuery, List<GroupRoleResponse>>
+    {
+        private readonly IGroupRoleRepository _groupRoleRepository;
+
+        public GroupRoleGetAllQueryHandler(IGroupRoleRepository groupRoleRepository)
+        {
+            _groupRoleRepository = groupRoleRepository;
+        }
+
+        public async Task<Result<List<GroupRoleResponse>>> Handle(GroupRoleGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var roles = request.Perdicate == null
+                ? await _groupRoleRepository.GetAllAsync(cancellationToken)
+                : await _groupRoleRepository.GetAllAsync(request.Perdicate, cancellationToken);
+
+            if (roles.IsFailure) return Result.Failure<List<GroupRoleResponse>>(roles);
+
+            return roles.Value.Select(r => new GroupRoleResponse(r)).ToList();
+        }
+    }
+}
