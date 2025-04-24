620b9ec80eac9cf47a4c99c5a3c235db9427b319
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-24 12:10:10 +0300

    Реализовал PersonalMessageGetAllQuery

---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalMessages/Queries/PersonalMessageGetAll/PersonalMessageGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Entity.User.Chat;
+using System.Linq.Expressions;
+
+namespace StudyTaskManager.Application.Entity.PersonalMessages.Queries.PersonalMessageGetAll;
+
+public sealed record PersonalMessageGetAllQuery(
+    Expression<Func<PersonalMessage, bool>>? Predicate) : IQuery<List<PersonalMessageResponse>>;
---
+++ StudyTaskManager/StudyTaskManager.Application/Entity/PersonalMessages/Queries/PersonalMessageGetAll/PersonalMessageGetAllQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using StudyTaskManager.Application.Abstractions.Messaging;
+using StudyTaskManager.Domain.Abstractions.Repositories;
+using StudyTaskManager.Domain.Shared;
+
+namespace StudyTaskManager.Application.Entity.PersonalMessages.Queries.PersonalMessageGetAll
+{
+    public class PersonalMessageGetAllQueryHandler : IQueryHandler<PersonalMessageGetAllQuery, List<PersonalMessageResponse>>
+    {
+        private readonly IPresonalMessageRepository _presonalMessageRepository;
+
+        public PersonalMessageGetAllQueryHandler(IPresonalMessageRepository presonalMessageRepository)
+        {
+            _presonalMessageRepository = presonalMessageRepository;
+        }
+
+        public async Task<Result<List<PersonalMessageResponse>>> Handle(PersonalMessageGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var messages = request.Predicate == null
+                ? await _presonalMessageRepository.GetAllAsync(cancellationToken)
+                : await _presonalMessageRepository.GetAllAsync(request.Predicate, cancellationToken);
+
+            if (messages.IsFailure) return Result.Failure<List<PersonalMessageResponse>>(messages);
+
+            return messages.Value.Select(pm => new PersonalMessageResponse(pm)).ToList();
+        }
+    }
+}
