b186e2b9c328f0bbc0bf78949960c04250d617d0
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-25 10:18:17 +0300

    Изменил возвращаемые данные у роли группы

--- StudyTaskManager/StudyTaskManager.Application/Entity/GroupRoles/Queries/GroupRoleResponse.cs
+++ StudyTaskManager/StudyTaskManager.Application/Entity/GroupRoles/Queries/GroupRoleResponse.cs
@@ -2,5 +2,18 @@
 
 namespace StudyTaskManager.Application.Entity.GroupRoles.Queries
 {
-    public sealed record GroupRoleResponse(GroupRole GroupRole);
+    public sealed record GroupRoleResponse(
+        Guid Id,
+        string Title,
+        bool CanCreateTasks,
+        bool CanManageRoles,
+        bool CanCreateTaskUpdates,
+        bool CanChangeTaskUpdates,
+        bool CanInviteUsers,
+        Guid? GroupId)
+    {
+        internal GroupRoleResponse(GroupRole role)
+            : this(role.Id, role.Title.Value, role.CanCreateTasks, role.CanManageRoles, role.CanCreateTaskUpdates, role.CanChangeTaskUpdates, role.CanInviteUsers, role.GroupId)
+        { }
+    }
 }
