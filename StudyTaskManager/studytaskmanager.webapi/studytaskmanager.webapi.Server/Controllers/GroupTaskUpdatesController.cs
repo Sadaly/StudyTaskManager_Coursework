80709404d126ed3de83e1cb5da36f29de2fe9f52
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-29 08:57:33 +0300

    Реализовал GroupTaskUpdatesController

---
+++ StudyTaskManager/studytaskmanager.webapi/studytaskmanager.webapi.Server/Controllers/GroupTaskUpdatesController.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudyTaskManager.Application.Entity.Groups.Queries.GroupGetAll;
+using StudyTaskManager.Application.Entity.Groups.Queries.GroupGetById;
+using StudyTaskManager.Application.Entity.GroupTaskUpdates.Commands.GroupTaskUpdateCreate;
+using StudyTaskManager.Application.Entity.GroupTaskUpdates.Commands.GroupTaskUpdateDelete;
+using StudyTaskManager.Application.Entity.GroupTaskUpdates.Commands.GroupTaskUpdateUpdate;
+using StudyTaskManager.Application.Entity.GroupTaskUpdates.Queries.GroupTaskUpdateGetAll;
+using StudyTaskManager.Application.Entity.GroupTaskUpdates.Queries.GroupTaskUpdateGetById;
+using StudyTaskManager.WebAPI.Abstractions;
+
+namespace StudyTaskManager.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class GroupTaskUpdatesController : ApiController
+    {
+        public GroupTaskUpdatesController(ISender sender) : base(sender) { }
+
+
+        //[Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            [FromBody] GroupTaskUpdateCreateCommand request,
+            CancellationToken cancellationToken)
+        {
+            var response = await Sender.Send(request, cancellationToken);
+
+            return response.IsSuccess ? Ok(response.Value) : BadRequest(response.Error);
+        }
+
+        //[Authorize]
+        [HttpDelete("{updateId:guid}")]
+        public async Task<IActionResult> Delete(
+            Guid updateId,
+            CancellationToken cancellationToken)
+        {
+            var request = new GroupTaskUpdateDeleteCommand(updateId);
+            var response = await Sender.Send(request, cancellationToken);
+
+            return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+        }
+
+        //[Authorize]
+        [HttpPut("{updateId:guid}")]
+        public async Task<IActionResult> Update(
+            Guid updateId,
+           [FromBody] string newContent,
+            CancellationToken cancellationToken)
+        {
+            var request = new GroupTaskUpdateUpdateCommand(updateId, newContent);
+            var response = await Sender.Send(request, cancellationToken);
+
+            return response.IsSuccess ? Ok(response) : BadRequest(response.Error);
+        }
+
+
+
+        //[Authorize]
+        [HttpGet("{updateId:guid}")]
+        public async Task<IActionResult> GetById(
+            Guid updateId,
+            CancellationToken cancellationToken)
+        {
+            var query = new GroupTaskUpdateGetByIdQuery(updateId);
+
+            var response = await Sender.Send(query, cancellationToken);
+
+            return response.IsSuccess ? Ok(response.Value) : NotFound(response.Error);
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAll(
+            CancellationToken cancellationToken)
+        {
+            var query = new GroupTaskUpdateGetAllQuery(null);
+
+            var response = await Sender.Send(query, cancellationToken);
+
+            return response.IsSuccess ? Ok(response.Value) : NotFound(response.Error);
+        }
+    }
+}
