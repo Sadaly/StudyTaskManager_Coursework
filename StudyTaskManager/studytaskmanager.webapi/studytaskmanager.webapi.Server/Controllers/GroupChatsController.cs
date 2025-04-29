265618f57ddf96fc71ff7bba5b0cf9b3d89bf46d
DP1415 <Daniil-2005-zima@mail.ru>
2025-04-29 21:13:39 +0300

    Добавил пустой GroupChatsController (нужно доделать слой приложения)

---
+++ StudyTaskManager/studytaskmanager.webapi/studytaskmanager.webapi.Server/Controllers/GroupChatsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudyTaskManager.WebAPI.Abstractions;
+
+namespace StudyTaskManager.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class GroupChatsController : ApiController
+    {
+        public GroupChatsController(ISender sender) : base(sender) { }
+
+
+
+    }
+}
