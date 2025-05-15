50169e1982b578b63872edbd6cd52a647626c4fe
DP1415 <Daniil-2005-zima@mail.ru>
2025-05-15 10:32:54 +0300

    Убрал лишнее, закоменченный код, лишние импорты (что были у меня)

--- StudyTaskManager/studytaskmanager.webapi/studytaskmanager.webapi.client/src/App.tsx
+++ StudyTaskManager/studytaskmanager.webapi/studytaskmanager.webapi.client/src/App.tsx
@@ -1,34 +1,17 @@
 import { useEffect, useState } from "react";
-import UsersList from "./Components/UsersList";
 import { UserResponse } from "./TypesFromTheServer/UserResponse";
-import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
+import WelcomePage from "./Pages/WelcomePage";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
-import WelcomePage from "./Pages/WelcomePage";
-import ProtectedRoute from './components/ProtectedRoute';
-import RoleProtectedRoute from './components/RoleProtectedRoute';
+import HomePage from "./Pages/HomePage";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
     const [users, setUsers] = useState<UserResponse[]>([])
 
-    //useEffect(() => {
-    //    fetchUsers()
-    //}, [])
-
-    //async function fetchUsers() {
-    //    try {
-    //        const response = await axios.get<UserResponse[]>('https://localhost:7241/api/Users/All')
-    //        setUsers(response.data)
-    //    } catch (e) {
-    //        alert(e)
-    //    }
-    //}
-
     return (
         <BrowserRouter>
-            {/*<UsersList users={users} />*/}
             <Routes>
                 <Route path="/" element={<WelcomePage />} />
                 <Route path="/login" element={<LoginPage />} />
