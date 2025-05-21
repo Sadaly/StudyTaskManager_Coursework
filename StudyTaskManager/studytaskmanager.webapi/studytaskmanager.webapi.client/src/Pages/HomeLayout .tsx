e3e16c5ffb37b473b3fd71814edae6cc829fcba8
DP1415 <Daniil-2005-zima@mail.ru>
2025-05-21 16:38:59 +0300

    Поднивнул кнопку выхода вниз

--- StudyTaskManager/studytaskmanager.webapi/studytaskmanager.webapi.client/src/Pages/HomeLayout .tsx
+++ StudyTaskManager/studytaskmanager.webapi/studytaskmanager.webapi.client/src/Pages/HomeLayout .tsx
@@ -3,30 +3,34 @@ import LogoutButton from "../Components/LogoutButton";
 
 const HomeLayout = () => {
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', height: '100vh' }}>
             <nav style={{
                 width: '15vw',
-                height: '90vh',
                 padding: '1rem',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between', // Распределяет пространство между элементами
+                height: '100%', // Занимает всю высоту родителя
             }}>
-                <h2>Меню</h2>
-                <ul
-                    style={{
+                <div>
+                    <h2>Меню</h2>
+                    <ul style={{
                         listStyle: 'none',
                         padding: 0
-                    }}
-                >
-                    <li><Link to="/home">Главная</Link></li>
-                    <li><Link to="/home/Chats">PersonalChats</Link></li>
-                    <li><Link to="/home/groups">Groups</Link></li>
-                </ul>
-                <LogoutButton />
+                    }}>
+                        <li><Link to="/home">Главная</Link></li>
+                        <li><Link to="/home/Chats">PersonalChats</Link></li>
+                        <li><Link to="/home/groups">Groups</Link></li>
+                    </ul>
+                </div>
+                <div style={{ marginTop: 'auto' }} >
+                    <LogoutButton />
+                </div>
             </nav>
 
             <div style={{
                 width: '80vw',
-                //marginTop: '20px',
-                //paddingTop: '20px'
+                padding: '1rem',
             }}>
                 <Outlet />
             </div>
